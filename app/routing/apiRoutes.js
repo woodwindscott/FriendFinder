312fef5e92331cadc598fa11857819e8c33d1be8
Scott Moore <woodwindscott@yahoo.com>
2019-10-02 17:38:27 -0400

    Final changes to post method, logic to find best match and modal to display results

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,6 +5,38 @@ var friendsList = require("../data/friends.js");
 
 var router = express.Router();
 
+var totalScore;
+var bestScore = 40;
+var bestMatchId = 0;
+
+function findMatch(newUser) {
+
+    for (var i = 0; i < friendsList.length; i++) {
+        compareScores(newUser, i);
+    }
+
+    console.log("BEST MATCH: User ID: " + bestMatchId + " - " + bestScore + " points!!!!!");
+
+} // End findMatch() function
+
+function compareScores(newUser, i) {
+
+    totalScore = 0;
+
+    for (var j = 0; j < 10; j++) {
+        totalScore += Math.abs(newUser.answers[j] - friendsList[i].answers[j]);
+    }
+
+    if (i === 0) {
+        bestScore = totalScore;
+        bestMatchId = i;
+    } else if (totalScore <= bestScore) {
+        bestScore = totalScore;
+        bestMatchId = i;
+    }
+
+} // End compareScores() function
+
 router.get("/api/friends", function(req, res) {
     return res.json(friendsList);
 });
@@ -19,8 +51,15 @@ router.post("/api/friends", function(req, res) {
             newUser.answers[i] = parseInt(newUser.answers[i]);
         }
 
+        findMatch(newUser);
+
         friendsList.push(newUser);
-        res.json(newUser);
+
+        res.send(
+            { bestMatchName: friendsList[bestMatchId].name,
+              bestMatchPhoto: friendsList[bestMatchId].photo
+            }
+        );
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
-
 var htmlRoutes = require("./app/routing/htmlRoutes.js");
 var apiRoutes = require("./app/routing/apiRoutes.js");
 
